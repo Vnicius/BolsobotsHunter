1817813d2c33e049b78f88fecc4cffa297aa3e44
Vinicius <vinicius.matheus252@gmail.com>
2019-04-05 19:29:09 -0300

    Update snif

--- snif.py
+++ snif.py
@@ -1,17 +1,41 @@
+import sys
 import argparse
 import re
 import pandas as pd
 
-parser = argparse.ArgumentParser()
-parser.add_argument('file', help='CSV file with the tweets')
-parser.add_argument('-o', '--output', help='Output file name', default='blacklist.csv')
-args = parser.parse_args()
+DEFAULT_OUTPUT = 'list.csv'
 
+def snif(input_file, output=DEFAULT_OUTPUT):
+    '''
+        Search for suspicious users in the tweets
 
-df = pd.read_csv(args.file, encoding='utf-8')
-users = df[['user_id', 'usernameTweet']]
-users_unique = users.drop_duplicates('user_id')
-bad_usernames = users[users['usernameTweet'].str.match(r'\w+\d{5,}')]
+        Params:
+            input_file (str): input file name
+            output (str): output file name
+    '''
+    # read the file
+    df = pd.read_csv(input_file, encoding='utf-8')
 
-bad_usernames.to_csv(args.output, encoding='utf-8', index=False)
+    if df.empty:
+        sys.exit('Empty file')
+    
+    # get only the users id and username
+    users = df[['user_id', 'usernameTweet']]
+    
+    # get only unique users
+    users_unique = users.drop_duplicates('user_id')
+    
+    # apply the filter to get suspecious users
+    bad_usernames = users[users['usernameTweet'].str.match(r'\w+\d{5,}')]
+
+    # save the result
+    bad_usernames.to_csv(output, encoding='utf-8', index=False)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file', help='CSV file with the tweets')
+    parser.add_argument('-o', '--output', help='Output file name', default=DEFAULT_OUTPUT)
+    args = parser.parse_args()
+
+    snif(args.file, args.output)
 
